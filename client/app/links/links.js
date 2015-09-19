90e39f9c509ea5df215897d913db3d627b6c243d
Joyce Liu <liujoycec@gmail.com>
2015-09-18 19:18:59 -0700

    Created http get request in Links factory which is then invoked in LinksController. Also added routing to /links and /shorten in app.js. Passing 11 out of 14 tests.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,17 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+
+  console.log('Links is ' + JSON.stringify(Links));
+  $scope.data = {};
+
+  $scope.getLinks = function() {
+    Links.allLinks().then(function(links) {
+      $scope.data.links = links;
+    });
+  }
+
+
+  $scope.getLinks();
+  // $scope.link = {};
 });
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,8 +1,23 @@
 angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
-  // Your code here
-})
+
+  var allLinks = function() {
+    return $http({
+      method: 'GET',
+      url: '/api/links'
+    }).then (function(response) {
+      console.log(response.data);
+      return response.data;
+    });
+  };
+
+  return {
+    allLinks: allLinks
+  };
+
+
+ })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
   // it is responsible for authenticating our user
